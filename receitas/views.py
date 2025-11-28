68a75627eb4fed7b328db3c7e429bfa50bf6ba8a
MatMnCn <coneglianmatheusg@gmail.com>
2025-11-28 18:50:16 -0300

    Aula 2 DJ

--- receitas/views.py
+++ receitas/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+a = "SENAI"
 
 def home(request):
-    return render(request, "home.html", context = 
-                {
-                    'nome': 'a',
-                })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render(request, "pages/home.html", {
+        'nome': a,
+    })
 
 def sobre(request):
     return HttpResponse("Sobre nós")
